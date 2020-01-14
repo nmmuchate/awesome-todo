c674fd6efd2170b2cbf976faa2a03d78490ba14a
nmmuchate <nicolas.muchate@gmail.com>
2020-01-14 08:32:41 +0200

    Initial commit

---
+++ src/router/routes.js
@@ -0,0 +1,28 @@
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('layouts/Layout.vue'),
+    children: [
+      { 
+        path: '/settings', 
+        component: () => import('pages/PaginaTODO.vue') }
+      },
+      path: '/settings', 
+        component: () => import('pages/PaginaTODO.vue') }
+      {
+
+      }
+    ]
+  }
+]
+
+// Always leave this as last one
+if (process.env.MODE !== 'ssr') {
+  routes.push({
+    path: '*',
+    component: () => import('pages/Error404.vue')
+  })
+}
+
+export default routes
