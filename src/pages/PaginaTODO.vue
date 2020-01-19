e898066933f324782eac9bf7a1302ff1bdc0b848
nmmuchate <nicolas.muchate@gmail.com>
2020-01-19 13:24:25 +0200

    adição de notificações.
    Uma pagina de notificação dentro da pagina todo

--- src/pages/PaginaTODO.vue
+++ src/pages/PaginaTODO.vue
@@ -1,12 +1,47 @@
 <template>
   <q-page padding>
- 
+    <q-list bordered padding>
+
+      <q-item tag="label" v-ripple>
+        <q-item-section side top>
+          <q-checkbox v-model="check1"/>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>Notifications</q-item-label>
+          <q-item-label caption>
+            Notify me about updates to apps or games
+            that I downloaded
+          </q-item-label>
+        </q-item-section>
+      </q-item>        
   </q-page>
 </template>
 
 <script>
 export default {
-  
+  data() {
+    return {
+      tasks: [
+        {
+          id: 1,
+          name: 'Vai para o mercado',
+          completed: false
+        },
+        {
+          id: 2,
+          name: 'Comprar Bananas',
+          completed: false
+        },
+        {
+          id: 3,
+          name: 'Comprar Maça',
+          completed: false
+        }
+
+      ]
+    }
+  }
 }
 </script>
 
