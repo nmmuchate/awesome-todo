78836e4cf8c8c1b68e3e223c42b302e487be51da
nmmuchate <nicolas.muchate@gmail.com>
2020-01-19 19:36:38 +0200

    adicao de um novo components

---
+++ src/components/Tasks/Modals/AddTask.vue
@@ -0,0 +1,21 @@
+<template>
+    <q-card>
+        <q-card-section>
+          <div class="text-h6">Alert</div>
+        </q-card-section>
+
+        <q-card-section>
+          frymys is a dounb
+        </q-card-section>
+
+        <q-card-actions align="right">
+          <q-btn flat label="OK" color="primary" v-close-popup/>
+        </q-card-actions>
+      </q-card>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
