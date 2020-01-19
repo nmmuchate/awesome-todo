36ca89fb8eeeb923c5cf8abdacec5c5ceeefdb0c
nmmuchate <nicolas.muchate@gmail.com>
2020-01-19 14:37:50 +0200

    adicao de datas, tempo e icon

--- src/pages/PaginaTODO.vue
+++ src/pages/PaginaTODO.vue
@@ -1,20 +1,33 @@
 <template>
-  <q-page padding>
-    <q-list bordered padding>
+  <q-page class="q-pa-md">
+    <q-list bordered>
 
-      <q-item tag="label" v-ripple>
+      <q-item 
+        v-for="task in tasks"
+        :key="task.id"
+        @click="task.completed = !task.completed"
+        clickable
+        v-ripple>
         <q-item-section side top>
-          <q-checkbox v-model="check1"/>
+          <q-checkbox v-model="task.completed" />
         </q-item-section>
 
         <q-item-section>
-          <q-item-label>Notifications</q-item-label>
-          <q-item-label caption>
-            Notify me about updates to apps or games
-            that I downloaded
-          </q-item-label>
+          <q-item-label>{{ task.name }}</q-item-label>
         </q-item-section>
-      </q-item>        
+
+        <q-item-section side top>
+          <q-icon name="event" /> 
+          <q-item-label caption>
+              {{ task.dueDate }}
+          </q-item-label>
+          <q-item-label caption>
+              {{ task.dueTime }}
+            </q-item-label>
+        </q-item-section>
+      </q-item>
+
+    </q-list>        
   </q-page>
 </template>
 
@@ -26,17 +39,23 @@ export default {
         {
           id: 1,
           name: 'Vai para o mercado',
-          completed: false
+          completed: false,
+          dueDate: '2020/01/19',
+          dueTime: '18:30'
         },
         {
           id: 2,
           name: 'Comprar Bananas',
-          completed: false
+          completed: false,
+           dueDate: '2020/01/19',
+           dueTime: '17:20'
         },
         {
           id: 3,
           name: 'Comprar Maça',
-          completed: false
+          completed: false,
+          dueDate: '20/01/19',
+          dueTime: '16:36'
         }
 
       ]
