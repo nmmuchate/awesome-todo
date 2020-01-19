55a5706fd2ca91c73f6a8f870d1009b33ba258d5
nmmuchate <nicolas.muchate@gmail.com>
2020-01-19 15:16:38 +0200

    mudanca de cor na pag.todo com cores

--- src/pages/PaginaTODO.vue
+++ src/pages/PaginaTODO.vue
@@ -1,11 +1,12 @@
 <template>
   <q-page class="q-pa-md">
-    <q-list bordered>
+    <q-list separator bordered>
 
       <q-item 
         v-for="task in tasks"
         :key="task.id"
         @click="task.completed = !task.completed"
+        :class="!task.completed ? 'bg-orange-1' : 'bg-green-1'"
         clickable
         v-ripple>
         <q-item-section side top>
@@ -13,19 +14,22 @@
         </q-item-section>
 
         <q-item-section>
-          <q-item-label>{{ task.name }}</q-item-label>
+          <q-item-label :class="{ 'text-strikethrough' : task.completed }">{{ task.name }}</q-item-label>
         </q-item-section>
 
         <q-item-section side>
           <div class="row">
-            <q-icon name="event" size="18px" />
-            <div class="column"></div> 
-            <q-item-label caption>
+            <div class="column justify-center">
+              <q-icon name="event" size="18px" class="q-mr-xs" />
+              </div>
+            <div class="column">
+            <q-item-label class="row justify-end" caption>
                 {{ task.dueDate }}
             </q-item-label>
-            <q-item-label caption>
+            <q-item-label class="row justify-end" caption>
                 {{ task.dueTime }}
             </q-item-label>
+            </div>
            </div>  
         </q-item-section>
       </q-item>
